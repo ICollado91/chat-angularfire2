5f72e499fb499296c665b2f814fb5e8aa3bd0935
ICollado91 <collado.ip@gmail.com>
2016-11-27 02:59:42 -0500

    login and logout with Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatRooms } from './chatrooms.component';
-import { Messages } from './message.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 // Must export the config
 export const firebaseConfig = {
@@ -15,11 +14,16 @@ export const firebaseConfig = {
     storageBucket: "bloc-chat-f9a61.appspot.com"
 };
 
+export const firebaseAuthConfig = {
+    provider: AuthProviders.Google,
+    method: AuthMethods.Popup
+}
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   declarations: [ 
       AppComponent,
--- src/app/chatrooms.component.ts
+++ src/app/chatrooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, AuthProviders, FirebaseListObservable } from 'angularfire2';
 
 @Component ({
     selector: 'chat-room',
@@ -12,15 +12,33 @@ export class ChatRooms {
     rooms: FirebaseListObservable<any[]>;
     messages: FirebaseListObservable<any[]>;
     currentRoom: string;
+    user = {};
+    loggedIn: boolean;
 
-    constructor(af: AngularFire) {
+    constructor(public af: AngularFire) {
         this.rooms = af.database.list('/Rooms');
         this.messages = af.database.list('/Messages');
         this.currentRoom = null;
+        this.af.auth.subscribe(user => {
+            if(user) {
+                //user logged in
+                this.user = user.auth.providerData[0].displayName;
+                this.loggedIn = true;
+            }
+            else {
+                //user not logged in
+                this.user = {};
+                this.loggedIn = false;
+            }
+        });
     }
     createRoom(newRoom: string){
-        this.rooms.push({name: newRoom});
+        this.rooms.push({
+            name: newRoom,
+            createdBy: this.user
+        });
         this.currentRoom = newRoom;
+        event.preventDefault();
     }
     readRoom(room: string) {
         this.currentRoom = room;
@@ -29,16 +47,24 @@ export class ChatRooms {
         this.rooms.remove(oldRoom);
     }
     createMessage(newMessage: string) {
-        if (!this.currentRoom) {
-            alert('Select a Room! WHAT IS WRONG WITH YOU?!')
-            return;
-        }
         this.messages.push({
             message: newMessage,
-            chatroom: this.currentRoom
+            chatroom: this.currentRoom,
+            createdBy: this.user
         });
+        event.preventDefault();
     }
     roomBoolean(room: string) {
-        return (this.currentRoom === room) ? true : false;
+        return (this.currentRoom === room);
+    }
+
+    login() {
+        this.af.auth.login({
+            provider: AuthProviders.Google
+        });
+    }
+
+    logout() {
+        this.af.auth.logout();
     }
 }
